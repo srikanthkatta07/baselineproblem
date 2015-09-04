6bc400a23a1b3688e39c261e1da7d33b0e3669f6
srikantk <kaanth.katta@gmail.com>
2015-09-04 17:03:48 +0530

    when two non imported basic need items are added to basket will return sum of their prices

--- src/com/thoughtworks/baseline/BillMachine.java
+++ src/com/thoughtworks/baseline/BillMachine.java
@@ -1,21 +1,26 @@
 //It should return total cost of the items
 package com.thoughtworks.baseline;
 
+import java.util.*;
+
 public class BillMachine {
 
     private String statement;
+    private double totalCost;
+    List<String> items = Arrays.asList("book", "packet of headache pills");
 
     public BillMachine(String statement) {
         this.statement = statement;
     }
 
-    public double getTotalCost() {
+    public double getTotalCost(double totalCost) {
         String[] receipt = statement.split(" ");
-        if (receipt[0] != "") {
+        if (statement.contains(items.get(0)) || statement.contains(items.get(1))) {
+            this.totalCost = totalCost;
             int numberOfItems = Integer.parseInt(receipt[0]);
             String itemName = receipt[1];
-            Double itemPrice = Double.parseDouble(receipt[3]);
-            return itemPrice;
+            Double itemPrice = Double.parseDouble(receipt[receipt.length - 1]);
+            return totalCost + itemPrice;
         }
         return 0.0;
     }
--- test/com/thoughtworks/baseline/BillMachineTest.java
+++ test/com/thoughtworks/baseline/BillMachineTest.java
@@ -10,14 +10,22 @@ public class BillMachineTest {
     public void shouldGiveZeroCostWhenNoItemsPresentInTheShoppingBasket() {
         BillMachine billMachine = new BillMachine("");
 
-        assertEquals(0.0, billMachine.getTotalCost(), 0.2d);
+        assertEquals(0.0, billMachine.getTotalCost(0), 0.2d);
     }
 
     @Test
-    public void shouldGivePriceOfTheItemWhichIsNonImportedBookWithOutSalesTax() {
+    public void shouldGivePriceOfTheItemWhichIsNonImportedBasicNeedItemWithOutSalesTax() {
         BillMachine billMachine = new BillMachine("1 book at 12.49");
 
-        assertEquals(12.49, billMachine.getTotalCost(), 0.2d);
+        assertEquals(12.49, billMachine.getTotalCost(0), 0.2d);
+
+    }
+
+    @Test
+    public void shouldGivePriceOfTheItemsWhenThoseAreNonImportedBasicNeedItemsPWithOutSalesTax() {
+        BillMachine billMachine = new BillMachine("1 book at 12");
+        BillMachine billMachine1=new BillMachine("1 packet of headache pills at 10");
+        assertEquals(22, billMachine1.getTotalCost(billMachine.getTotalCost(0)), 0.2d);
 
     }
 }
