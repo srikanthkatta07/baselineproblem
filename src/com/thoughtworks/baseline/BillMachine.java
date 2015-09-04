103a1d7c0b31aa3fa7c4f03df94b5e6b4f5c71b4
srikantk <kaanth.katta@gmail.com>
2015-09-04 16:38:36 +0530

    shoudl give item price without sales tax  when non imported item book is purchased

--- src/com/thoughtworks/baseline/BillMachine.java
+++ src/com/thoughtworks/baseline/BillMachine.java
@@ -3,7 +3,20 @@ package com.thoughtworks.baseline;
 
 public class BillMachine {
 
+    private String statement;
+
+    public BillMachine(String statement) {
+        this.statement = statement;
+    }
+
     public double getTotalCost() {
+        String[] receipt = statement.split(" ");
+        if (receipt[0] != "") {
+            int numberOfItems = Integer.parseInt(receipt[0]);
+            String itemName = receipt[1];
+            Double itemPrice = Double.parseDouble(receipt[3]);
+            return itemPrice;
+        }
         return 0.0;
     }
 }
--- test/com/thoughtworks/baseline/BillMachineTest.java
+++ test/com/thoughtworks/baseline/BillMachineTest.java
@@ -8,8 +8,16 @@ public class BillMachineTest {
 
     @Test
     public void shouldGiveZeroCostWhenNoItemsPresentInTheShoppingBasket() {
-        BillMachine billMachine = new BillMachine();
+        BillMachine billMachine = new BillMachine("");
 
         assertEquals(0.0, billMachine.getTotalCost(), 0.2d);
+    }
+
+    @Test
+    public void shouldGivePriceOfTheItemWhichIsNonImportedBookWithOutSalesTax() {
+        BillMachine billMachine = new BillMachine("1 book at 12.49");
+
+        assertEquals(12.49, billMachine.getTotalCost(), 0.2d);
+
     }
 }
